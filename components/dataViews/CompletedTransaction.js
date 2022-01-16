afed6358bfec4e2637019de59d2700e344f88270
samepant <sampanter@gmail.com>
2022-01-16 17:51:43 -0500

    converts classes to functions to better make use of the context provider

--- components/dataViews/CompletedTransaction.js
+++ components/dataViews/CompletedTransaction.js
@@ -1,45 +1,49 @@
 import React from "react";
 
+import { useAppContext } from "../../context/AppContext";
 import StackableContainer from "../layout/StackableContainer";
 import HashView from "./HashView";
 import Button from "../inputs/Button";
 
-const CompletedTransaction = ({ transactionHash }) => (
-  <StackableContainer lessPadding lessMargin>
-    <StackableContainer lessPadding lessMargin lessRadius>
-      <div className="confirmation">
-        <svg viewBox="0 0 77 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 30L26 51L72 5" stroke="white" strokeWidth="12" />
-        </svg>
-        <p>This transaction has been broadcast.</p>
-      </div>
+const CompletedTransaction = ({ transactionHash }) => {
+  const { state } = useAppContext();
+  return (
+    <StackableContainer lessPadding lessMargin>
+      <StackableContainer lessPadding lessMargin lessRadius>
+        <div className="confirmation">
+          <svg viewBox="0 0 77 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 30L26 51L72 5" stroke="white" strokeWidth="12" />
+          </svg>
+          <p>This transaction has been broadcast.</p>
+        </div>
+      </StackableContainer>
+      <StackableContainer lessPadding lessMargin lessRadius>
+        <label>Transaction Hash</label>
+        <HashView hash={transactionHash} />
+      </StackableContainer>
+      {state.chain.chainID === "cosmoshub-4" && (
+        <Button
+          href={`https://www.mintscan.io/cosmos/txs/${transactionHash}`}
+          label=" View on Mintscan"
+        ></Button>
+      )}
+      <style jsx>{`
+        .confirmation {
+          display: flex;
+          justify-content: center;
+        }
+        label {
+          font-size: 12px;
+          font-style: italic;
+          margin-bottom: 0.5em;
+        }
+        .confirmation svg {
+          height: 0.8em;
+          margin-right: 0.5em;
+        }
+      `}</style>
     </StackableContainer>
-    <StackableContainer lessPadding lessMargin lessRadius>
-      <label>Transaction Hash</label>
-      <HashView hash={transactionHash} />
-    </StackableContainer>
-    {process.env.NEXT_PUBLIC_CHAIN_ID === "cosmoshub-4" && (
-      <Button
-        href={`https://www.mintscan.io/cosmos/txs/${transactionHash}`}
-        label=" View on Mintscan"
-      ></Button>
-    )}
-    <style jsx>{`
-      .confirmation {
-        display: flex;
-        justify-content: center;
-      }
-      label {
-        font-size: 12px;
-        font-style: italic;
-        margin-bottom: 0.5em;
-      }
-      .confirmation svg {
-        height: 0.8em;
-        margin-right: 0.5em;
-      }
-    `}</style>
-  </StackableContainer>
-);
+  );
+};
 
 export default CompletedTransaction;
